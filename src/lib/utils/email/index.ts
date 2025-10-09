7c56f72bc593ece8e46920c2f681c2aac02bf4e6
ehab <ftwehab@gmail.com>
2025-10-09 03:50:40 +0300

    update variables

--- src/lib/utils/email/index.ts
+++ src/lib/utils/email/index.ts
@@ -4,14 +4,14 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   auth: {
-    user: "ftwehab@gmail.com",
+    user: process.env.SENDER,
     pass: process.env.PASSWORD,
   },
 });
 
 async function sendEmail({ from, subject, html }: SendMailOptions) {
   transporter.sendMail({
-    from: `${from} <ftwehab@gmail.com>`,
+    from: `${from} <${process.env.SENDER}>`,
     subject: `Email from ${subject}`,
     to: "ftwehab@gmail.com",
     html,
