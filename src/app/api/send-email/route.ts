95f661fac87f016f715b73cd81b5d28fd02689aa
ehab <ftwehab@gmail.com>
2025-10-09 08:57:14 +0300

    update send email

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,10 +1,25 @@
-import sendEmail from "@/lib/utils/email";
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(req: NextRequest) {
   try {
     const { from, subject, html } = await req.json();
-    sendEmail({ from, subject, html });
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SENDER,
+        pass: process.env.PASSWORD,
+      },
+    });
+    await transporter.sendMail({
+      from: `${from} <${process.env.SENDER}>`,
+      subject: `Email from ${subject}`,
+      to: process.env.SENDER,
+      html,
+    });
+
     return NextResponse.json({ success: true });
   } catch (error) {
     const errorMessage =
